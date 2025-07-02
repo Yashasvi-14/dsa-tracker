106aa55d3e1290045b39c2afc7e96dc66047947e
Yashasvi-14 <Yashasvi-14@users.noreply.github.com>
2025-07-02 20:34:28 +0530

    installed cros and updated .env for render deployment

--- dsa-tracker-backend/index.js
+++ dsa-tracker-backend/index.js
@@ -3,6 +3,7 @@ const express=require('express');
 const mongoose=require('mongoose');
 const dotenv=require('dotenv');
 const path = require("path");
+const cors=require('cors');
 
 const app=express();
 dotenv.config();
@@ -10,7 +11,9 @@ dotenv.config();
 const connectDB=require('./config/db');
 connectDB();
 
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 // Serve frontend static files
 app.use(express.static(path.join(__dirname, "frontend")));
